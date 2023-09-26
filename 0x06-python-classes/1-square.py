a1338d000f8893db554f2cf27925fb1f3385735d
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-09-26 11:52:35 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 class Square:
     """Class Square that defines a square with a private instance attribute size."""
+    # This is the docstring for the class
 
     def __init__(self, size):
         """Initialize a new Square.
-
         Args:
             size (int): The size of the new square.
         """
+        # This is the docstring for the method
         self.__size = size
