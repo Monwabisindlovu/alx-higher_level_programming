b1ae430350124331328a2f0ab661a1ccc782c016
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-02 15:33:02 +0000

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python3
-"""Module for the Rectangle class."""
+"""
+This module defines a Rectangle class.
 
+The Rectangle class defines a rectangle by its width and height, which are 
+both integers greater than or equal to 0. The class also includes methods 
+for calculating the area and perimeter of the Rectangle.
+"""
 
 class Rectangle:
-    """Defines a rectangle."""
+    """
+    This class defines a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle. Must be an integer greater than or equal to 0.
+        height (int): The height of the rectangle. Must be an integer greater than or equal to 0.
+    """
+
     def __init__(self, width=0, height=0):
-        """Initializes a new instance of the Rectangle class."""
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int, optional): The width of the rectangle. Defaults to 0.
+            height (int, optional): The height of the rectangle. Defaults to 0.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter method for the width attribute."""
+        """
+        Getter for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter method for the width attribute."""
+        """
+        Setter for the width attribute.
+
+        Args:
+            value (int): The width of the rectangle.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If the width is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,12 +57,26 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter method for the height attribute."""
+        """
+        Getter for the height attribute.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method for the height attribute."""
+        """
+        Setter for the height attribute.
+
+        Args:
+            value (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the height is not an integer.
+            ValueError: If the height is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -38,25 +84,27 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Calculates and returns the rectangle area."""
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates and returns the rectangle perimeter."""
+        """
+        Calculates the perimeter of the rectangle.
+
+        Returns:
+            int: The perimeter of the rectangle.
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
-    def __str__(self):
-        """String representation of the rectangle using '#' character."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
-
-    def __repr__(self):
-        """String representation of the object for eval() support."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
     def __del__(self):
-        """Destructor method to print 'Bye rectangle...' when an instance is deleted."""
+        """
+        Deletes the rectangle instance.
+        """
         print("Bye rectangle...")
