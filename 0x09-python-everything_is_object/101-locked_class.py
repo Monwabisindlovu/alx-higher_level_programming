26eaae4e4cf97e1d9b50c5891b9a67418cbdc846
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-03 15:21:52 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module that defines a class with restricted attribute assignment.
+"""
+
 class LockedClass:
     """
-    A class that locks down attribute assignments.
+    A class that only allows the creation of a 'first_name' attribute.
 
-    This class is designed to only allow the creation of the 'first_name' attribute.
-    Any attempt to set any other attribute will raise an AttributeError.
+    This class is designed to restrict dynamic attribute creation, allowing only
+    the 'first_name' attribute to be set. Any other attribute assignment will raise
+    an AttributeError.
     """
 
     __slots__ = ['first_name']
