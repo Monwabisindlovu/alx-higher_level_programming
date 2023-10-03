bf872230eabfafec852ce843663f6847eee9890c
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-03 15:04:18 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
-    """
-    This function returns the string "BestSchool" repeated n times,
-    where n is the number of times the function has been called.
-
-    Returns:
-        str: The string "BestSchool" repeated n times.
-    """
-    if not hasattr(magic_string, 'counter'):
-        magic_string.counter = -1
-    magic_string.counter += 1
-    return "BestSchool, " * magic_string.counter + "BestSchool"
-
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
