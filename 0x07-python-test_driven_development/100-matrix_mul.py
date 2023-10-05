fd50e5fc2d9b6bad5abc50b041a3509243fa8cc9
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-05 02:15:01 -0700

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,52 +1,43 @@
 #!/usr/bin/python3
-"""Matrix multiplication module.
+# 101-lazy_matrix_mul.py
+"""Defines a matrix multiplication function using NumPy."""
 
-This module defines a function to multiply two matrices.
+import numpy as np
 
-Attributes:
-    matrix_mul: Function to multiply two matrices.
-
-"""
-
-def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
+def lazy_matrix_mul(m_a, m_b):
+    """Multiply two matrices using NumPy.
 
     Args:
         m_a (list of lists): The first matrix.
         m_b (list of lists): The second matrix.
 
-    Returns:
-        list of lists: The result of matrix multiplication.
-
     Raises:
-        TypeError: If m_a or m_b is not a list of lists, contains non-numeric elements, or is not rectangular.
-        ValueError: If m_a or m_b is empty or cannot be multiplied.
+        ValueError: If the matrices cannot be multiplied due to incompatible dimensions.
+        TypeError: If the matrices contain invalid data, e.g., non-numeric elements.
 
+    Returns:
+        list of lists: The result of matrix multiplication as a list of lists.
+
+    Example:
+
+    >>> m_a = [[1, 2], [3, 4]]
+    >>> m_b = [[1, 2], [3, 4]]
+    >>> result = lazy_matrix_mul(m_a, m_b)
+    >>> print(result)
+    [[ 7 10]
+     [15 22]]
     """
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a must be a list and m_b must be a list")
+    try:
+        np_m_a = np.array(m_a)
+        np_m_b = np.array(m_b)
+        result = np.matmul(np_m_a, np_m_b)
+        return result.tolist()
+    except ValueError:
+        raise ValueError("Matrices cannot be multiplied due to incompatible dimensions")
+    except TypeError:
+        raise TypeError("Matrices must contain only integers or floats")
 
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a must be a list of lists and m_b must be a list of lists")
-
-    if not m_a or not m_b:
-        raise ValueError("m_a can't be empty and m_b can't be empty")
-
-    if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
-        raise TypeError("m_a should contain only integers or floats and m_b should contain only integers or floats")
-
-    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_a must be of the same size and each row of m_b must be of the same size")
-
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
-    
-    for i in range(len(m_a)):
-        for j in range(len(m_b[0])):
-            for k in range(len(m_b)):
-                result[i][j] += m_a[i][k] * m_b[k][j]
-
-    return result
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
