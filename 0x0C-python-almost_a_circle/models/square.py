34b5a6de6350bfecf36934893dfeffd5c8719b8e
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-10-16 10:05:46 -0700

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Square module"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for Square class."""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Method to return a string representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Method to update attributes using positional and keyword arguments."""
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
