b25c7c5cd437c8ac16d6a89c9ad3b4e897432d76
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-15 03:13:22 -0800

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 13-count_shows_by_genre.sql)
-SELECT tv_genres.name
-FROM tv_show_genres
-        JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-        JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name ASC;
+-- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
+  FROM tv_genres
+  INNER JOIN tv_show_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  GROUP BY tv_genres.name
+  ORDER BY number_of_shows DESC;
