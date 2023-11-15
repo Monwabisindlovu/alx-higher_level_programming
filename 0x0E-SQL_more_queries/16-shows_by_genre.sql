61fc6b4f927d6a04b8c274621f57840d734de536
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-15 03:34:54 -0800

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
--- List all shows and their corresponding genres.
--- This script selects the title from tv_shows and the name from tv_genres,
--- joining them on tv_show_genres.show_id and tv_shows.id.
-SELECT
-    tv_shows.title AS title,
-    tv_genres.name AS name
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id;
+-- List all shows and their genres, including those without genres, ordered by show title and genre name.
+-- This script selects the title from tv_shows, and the genre name from tv_genres using left joins with tv_show_genres
+-- based on the show and genre IDs. It orders the result by show title and genre name.
+SELECT ts.title, tgg.name
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tgs ON ts.id = tgs.show_id
+LEFT JOIN tv_genres AS tgg ON tgs.genre_id = tgg.id
+ORDER BY ts.title, tgg.name;
+
