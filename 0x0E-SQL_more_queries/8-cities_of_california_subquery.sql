0972ec4a16f0d9d9617e3838063f72567c2262a3
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-15 03:36:59 -0800

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,15 +1,4 @@
--- Use the database
-USE hbtn_0d_usa;
-
--- Identify duplicate entries for 'California'
-SELECT id, name FROM states WHERE name = 'California';
-
--- Delete duplicate entries for 'California' except the one with the desired ID
-DELETE FROM states WHERE id = 1;
-
--- Verify that there is only one entry for 'California'
-SELECT * FROM states WHERE name = 'California';
-
--- Now, run the original script
-SOURCE 8-cities_of_california_subquery.sql;
-
+-- cities of california exercise
+SELECT c.id, c.name FROM cities c, states s
+WHERE c.id = s.id
+ORDER BY c.id ASC;
