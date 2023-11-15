6a5f556b6f38cad2a5b0dca09adf938d3e3f3f3f
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-15 02:57:08 -0800

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++
@@ -1,9 +0,0 @@
--- Use the database
-USE hbtn_0d_tvshows_rate;
-
--- Select shows and their rating sum
-SELECT tv_shows.title, SUM(rating) AS rating
-FROM tv_shows
-JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating DESC;
