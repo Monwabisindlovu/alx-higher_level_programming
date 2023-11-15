ba3ae2f816499af94309c82470cd9026aed96f02
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-15 03:53:06 -0800

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,7 @@
--- List all show titles that belong to the genre 'Comedy' by joining tv_shows, tv_show_genres, and tv_genres.
--- The result is filtered using WHERE to include only shows with the genre 'Comedy' and is ordered by title.
-SELECT ts.title
-FROM tv_shows AS ts
-JOIN tv_show_genres AS tgs ON ts.id = tgs.show_id
-JOIN tv_genres AS tgg ON tgs.genre_id = tgg.id
-WHERE tgg.name = 'Comedy'
-GROUP BY ts.title ASC;
-
+-- Script that lists shows with the genre 'Comedy'
+SELECT tv_shows.title 
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy' 
+ORDER BY tv_shows.title;
