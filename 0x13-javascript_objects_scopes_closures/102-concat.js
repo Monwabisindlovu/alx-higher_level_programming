61145eb6d6225bd03ab11a4f4fe33799267b10d3
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-12 02:52:57 -0800

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const fs = require(fs);
-const a = fs.readFileSync(process.argv[2], utf8);
-const b = fs.readFileSync(process.argv[3], utf8);
-fs.writeFileSync(process.argv[4], a + b);
+/** Write a script that concats 2 files. */
+const argv = require('process').argv;
+const fs = require('fs');
+const firstData = fs.readFileSync('./' + argv[2]);
+const secondData = fs.readFileSync('./' + argv[3]);
+fs.writeFileSync('./' + argv[4], firstData + secondData);
