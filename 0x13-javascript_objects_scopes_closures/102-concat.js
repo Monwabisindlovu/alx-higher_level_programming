72c164bf63d110226dbad685a81f60ae7f58197e
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-12 02:24:32 -0800

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,38 +1,15 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
+// Get the file paths from the command line arguments
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-fs.readFile(fileA, 'utf8', (errA, dataA) => {
-  if (errA) {
-    console.error(errA.message);
-    process.exit(1);
-  }
+// Read the contents of fileA and fileB
+const contentA = fs.readFileSync(fileA, 'utf8');
+const contentB = fs.readFileSync(fileB, 'utf8');
 
-  fs.readFile(fileB, 'utf8', (errB, dataB) => {
-    if (errB) {
-      console.error(errB.message);
-      process.exit(1);
-    }
-
-    const concatenatedContent = dataA + dataB;
-
-    fs.writeFile(fileC, concatenatedContent, (errC) => {
-      if (errC) {
-        console.error(errC.message);
-        process.exit(1);
-      }
-
-      console.log(`The content of ${fileA} and ${fileB} has been successfully concatenated to ${fileC}`);
-    });
-  });
-});
+// Concatenate the contents and write to fileC
+fs.writeFileSync(fileC, contentA + '\n' + contentB);
 
