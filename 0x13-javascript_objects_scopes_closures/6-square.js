e58e08e28f2ea152c8bdd1740f9b4fe97716a6a0
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-12 02:46:27 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
-const Square = require('./5-square.js').Square;
 
-Square.prototype.charPrint = function (c) {
-  if (c === undefined) {
-    c = 'X';
-  }
-  for (let i = 0; i < this.height; i++) {
-    console.log((c).repeat(this.width));
-  }
-};
+const Square = require('./5-square');
 
-module.exports = {Square};
+class SquareWithCharPrint extends Square {
+  charPrint(c) {
+    // If c is undefined, use the character X
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    // Print the square using the specified character c
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = SquareWithCharPrint;
