6b721180535459cf1682e63c32db47f5a0735805
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-12 02:33:37 -0800

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,6 @@
 
 const list = require('./100-data').list;
 
-// Use map to create a new list with each value multiplied by its index
 const newList = list.map((value, index) => value * index);
 
 console.log(list);
