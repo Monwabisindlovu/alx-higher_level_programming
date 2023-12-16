3f302071e6563154940d46e9fdaf38ea83d03509
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-12-15 20:42:01 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays
+all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name='{}'\
+            ORDER BY id ASC".format(sys.argv[4])
+        cur.execute(query)
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
-
-    cur = db.cursor()
-
-    cur.execute("""
-        SELECT * FROM states
-        WHERE name = %s
-        ORDER BY id ASC
-    """, (state_name,))
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: %s" % str(e))
 
