aa5401090b0744fec42f0cc41abe067f44f19d09
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-05 03:04:14 -0800

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
-curl -sL -w "%{http_code}" "$1" -o /dev/null
-
+curl -sL "$1"
