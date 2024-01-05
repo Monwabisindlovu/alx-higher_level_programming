cb1cde9e69b78aa4489ad865321e41a40b091762
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-05 10:17:10 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me and cause the server to respond with "You got me!"
-curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
 
