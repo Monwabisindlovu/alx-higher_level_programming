b2341c2174616e51867cf0381dbf4906d87a46c0
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-05 02:22:11 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script sends a request to a URL and displays the size of the body of the response
-curl -s "$1" | wc -c
+# This script takes a URL, sends a request, and displays the size of the response body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
 
