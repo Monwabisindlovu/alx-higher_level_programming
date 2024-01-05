a4ff12e4b954cb66ec0b1beb2b4440747f15b4ba
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-05 02:49:18 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script takes a URL, sends a request, and displays the size of the response body in bytes
+echo "Script started"
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
 
