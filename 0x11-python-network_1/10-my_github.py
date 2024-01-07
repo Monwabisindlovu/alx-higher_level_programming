259b42bad251c2dd60c2a1ef8f4be4ce8b4ef3e1
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-07 01:27:03 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """
-Uses Basic Authentication with a personal access token to access the GitHub API
-and displays the user id.
-Usage: ./10-my_github.py <username> <token>
+Uses the GitHub API to display a GitHub ID based on given credentials.
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
 """
-
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    # Extract username and token from command-line arguments
     username = sys.argv[1]
-    token = sys.argv[2]
+    password = sys.argv[2]
 
-    # Set up the authentication headers using Basic Authentication with the token
-    headers = {'Authorization': 'Basic {}:{}'.format(username, token)}
+    auth = HTTPBasicAuth(username, password)
 
-    # Make a GET request to the GitHub API to retrieve user information
-    response = requests.get('https://api.github.com/user', headers=headers)
+    response = requests.get('https://api.github.com/user', auth=auth)
 
     try:
-        # Try to parse the JSON response
         json_data = response.json()
-        # Display the user id
         print(json_data['id'])
     except ValueError:
-        # Handle the case where the response is not a valid JSON
         print(None)
-
