8ff6070ea2a6b25a0ce7f64d9f60da642553253e
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-06 22:46:20 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
+"""
+Module 0-hbtn_status.py
+"""
 
 import urllib.request
 
-if __name__ == "__main__":
-    # Open a connection to the URL
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        # Read the response body as bytes
-        body = response.read()
-        # Print the body information
+def fetch_and_display_status(url):
+    """
+    Fetches and displays information from the given URL.
+    """
+    with urllib.request.urlopen(url) as response:
+        response_content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(response_content)))
+        print("\t- content: {}".format(response_content))
+        print("\t- utf8 content: {}".format(response_content.decode("utf-8")))
+
+if __name__ == "__main__":
+    # Specify the URL
+    status_url = 'https://alx-intranet.hbtn.io/status'
+    fetch_and_display_status(status_url)
 
