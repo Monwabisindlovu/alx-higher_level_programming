7e73a0ee260a9f63bc4a887f731d07f10ab530e1
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-07 21:03:01 -0800

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Module 2-post_email.py
-"""
-
 import urllib.request
 import urllib.parse
-import sys
+from sys import argv
+"""
+script that takes in a url and an email, sends a POST request
+to the url with the email as parameter and displays body of the
+response
+"""
 
-def post_email(url, email):
-    """
-    Sends a POST request with the given email to the specified URL.
-    Displays the body of the response.
-    """
-    # Encode the email parameter
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-    # Make a POST request to the URL with the encoded data
-    with urllib.request.urlopen(url, data=data) as response:
-        response_body = response.read().decode('utf-8')
-        print(response_body)
 
 if __name__ == "__main__":
-    # Check if both URL and email are provided as command-line arguments
-    if len(sys.argv) != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    print("Email: {}".format(email))  # Adjusted the output format
-    post_email(url, email)
-
+    """
+    """
+    info = {'email': argv[2]}
+    data = urllib.parse.urlencode(info)
+    data = data.encode('ascii')
+    url = argv[1]
+    reply = urllib.request.Request(url, data)
+    with urllib.request.urlopen(reply) as rep:
+        body = rep.read()
+    print(body.decode(encoding="utf-8"))
