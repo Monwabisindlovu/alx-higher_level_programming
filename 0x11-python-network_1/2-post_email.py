b1959a5363e537bd8198bb6e01c199ce2ed5ff73
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-07 21:05:24 -0800

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
+"""handling post request"""
 import urllib.request
 import urllib.parse
-from sys import argv
-"""
-script that takes in a url and an email, sends a POST request
-to the url with the email as parameter and displays body of the
-response
-"""
+import sys
 
 
 if __name__ == "__main__":
-    """
-    """
-    info = {'email': argv[2]}
-    data = urllib.parse.urlencode(info)
-    data = data.encode('ascii')
-    url = argv[1]
-    reply = urllib.request.Request(url, data)
-    with urllib.request.urlopen(reply) as rep:
-        body = rep.read()
-    print(body.decode(encoding="utf-8"))
+    email = sys.argv[2]
+    url = sys.argv[1]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    request = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(request) as res:
+        print(res.read().decode('utf-8'))
