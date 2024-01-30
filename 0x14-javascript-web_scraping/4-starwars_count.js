8d9710489f70587aa93830c6ce52bc7653734bb3
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-30 04:11:22 -0800

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,44 +1,32 @@
 #!/usr/bin/node
-/* A script that prints the number of movies where the character "Wedge Antilles" is present. */
-
+/**
+  prints number of movies where the character 'Wedge Antilles' is present.
+  usage: ./4-starwars_count.js <API URL>
+  */
+const myArgs = process.argv.slice(2);
 const request = require('request');
-
-// Get the Star Wars API URL from the command line arguments
-const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error('Please provide the Star Wars API URL');
-  process.exit(1);
-}
-
-// Define the character ID for "Wedge Antilles"
-const characterId = 18;
-
-// Make a GET request to the Star Wars API
-request(apiUrl, (error, response, body) => {
+const characters_ = [];
+request(myArgs[0], function (error, response, body) {
   if (error) {
-    console.error(error);
-  } else {
-    try {
-      // Parse the JSON response
-      const filmsData = JSON.parse(body);
+    console.log(error);
+  }
+  if (!error) {
+    let i = 0;
+    const json_ = JSON.parse(body);
 
-      // Validate the response
-      if (!filmsData || !Array.isArray(filmsData.results)) {
-        console.error('Invalid API response');
-        return;
-      }
-
-      // Filter films where "Wedge Antilles" is present
-      const filmsWithWedge = filmsData.results.filter((film) => {
-        return Array.isArray(film.characters) && film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-      });
-
-      // Print the number of films with "Wedge Antilles"
-      console.log(filmsWithWedge.length);
-    } catch (err) {
-      console.error('Error parsing API response:', err);
+    for (i = 0; i < 7; i++) {
+      characters_.push(json_.results[i].characters);
     }
   }
+  let counter = 0;
+  characters_.forEach(function each (item) {
+    if (Array.isArray(item)) {
+      item.forEach(each);
+    } else {
+      if (item === 'https://swapi-api.hbtn.io/api/people/18/') {
+        counter++;
+      }
+    }
+  });
+  console.log(counter);
 });
-
