b62c13b78ad0feca8135c6d94ed436efbe9a261e
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-30 04:29:44 -0800

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,45 +1,30 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
- * Displays one character name per line in the same order as the list in the /films/ response
- * Uses the Star Wars API
- * Uses the module 'request'
- */
-
+// A script that prints all characters of a Star Wars movie
+const movieId = process.argv.slice(2)[0];
 const request = require('request');
 
-// Get the Movie ID from the command line arguments
-const movieId = process.argv[2];
+const filmsUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Define the Star Wars API endpoint for films
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+function printCharacterName (characters, index) {
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const name = JSON.parse(body).name;
+      console.log(name);
+      if (index < characters.length - 1) {
+        printCharacterName(characters, index + 1);
+      }
+    }
+  });
+}
 
-// Make a GET request to the Star Wars API for the specified film
-request(apiUrl, (error, response, body) => {
+request(filmsUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
-    // Parse the JSON response
-    const filmData = JSON.parse(body);
-
-    // Iterate through the character URLs of the movie and print in order
-    filmData.characters.forEach((characterUrl, index) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error(charError);
-        } else {
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-          
-          // Check if it's the last character to print
-          if (index === filmData.characters.length - 1) {
-            // All characters have been printed
-            process.exit();
-          }
-        }
-      });
-    });
+    const parseData = JSON.parse(body);
+    const characters = parseData.characters;
+    printCharacterName(characters, 0);
   }
 });
-
